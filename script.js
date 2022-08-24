6a2ff794ad0832a71813b1e4e5aa6663fb2d0197
Juan Cruz Engel <jcruzengel@gmail.com>
2022-08-23 22:36:02 -0300

    Optimizando la calculadora

--- script.js
+++ script.js
@@ -18,84 +18,84 @@
 // let primerNumero = parseFloat(prompt("Ingrese un numero"))
 // let segundoNumero = parseFloat(prompt("Ingrese otro numero"))
 
-window.addEventListener('load', ()=> {
-    const display = document.querySelector('.calculatordisplay')
-    const keypadButtons = document.getElementsByClassName('keypad-button')
+// window.addEventListener('load', ()=> {
+//     const display = document.querySelector('.calculatordisplay')
+//     const keypadButtons = document.getElementsByClassName('keypad-button')
 
-    const keypadButtonsArray = Array.from(keypadButtons)
+//     const keypadButtonsArray = Array.from(keypadButtons)
     
-    keypadButtonsArray.forEach( (button) => {
-        button.addEventListener('click', ()=> {
-           calculadora(button, display)
-        })
-    })
-});
+//     keypadButtonsArray.forEach( (button) => {
+//         button.addEventListener('click', ()=> {
+//            calculadora(button, display)
+//         })
+//     })
+// });
 
-function calculadora(button, display) {
-    switch (button.innerHTML) {
-        case 'C':
-            borrar(display)
-            break;
-        case '=':
-            calcular(display)
-            break;
+// function calculadora(button, display) {
+//     switch (button.innerHTML) {
+//         case 'C':
+//             borrar(display)
+//             break;
+//         case '=':
+//             calcular(display)
+//             break;
         
-        default:
-            actualizar(display)
-            break;
-    }
-}
+//         default:
+//             actualizar(display)
+//             break;
+//     }
+// }
 
-function calcular(display) {
-    display.innerHTML = eval(display.innerHTML)
-}
+// function calcular(display) {
+//     display.innerHTML = eval(display.innerHTML)
+// }
 
-function actualizar(display, button) {
-    if (display.innerHTML == 0) {
-        display.innerHTML = ''
-    }
-    display.innerHTML += button.innerHTML
-}
+// function actualizar(display, button) {
+//     if (display.innerHTML == 0) {
+//         display.innerHTML = ''
+//     }
+//     display.innerHTML += button.innerHTML
+// }
 
-function borrar(display) {
-    display.innerHTML = 0
-}
+// function borrar(display) {
+//     display.innerHTML = 0
+// }
 
-const sumar = (num1, num2) => num1 + num2
-const restar = (num1, num2) => num1 - num2
-const multiplicar = (num1, num2) => num1 * num2
-const dividir = (num1, num2) => num1 / num2
+// const sumar = (num1, num2) => num1 + num2
+// const restar = (num1, num2) => num1 - num2
+// const multiplicar = (num1, num2) => num1 * num2
+// const dividir = (num1, num2) => num1 / num2
 
-let numero1, numero2, operacion
+// let numero1, numero2, operacion
 
-do {
-    numero1 = parseFloat(prompt("Ingrese un numero"))
-    numero2 = parseFloat(prompt("Ingrese otro numero"))
-    operacion = prompt("Ingrese una operacion")
+// // do {
+// //     numero1 = parseFloat(prompt("Ingrese un numero"))
+// //     numero2 = parseFloat(prompt("Ingrese otro numero"))
+// //     operacion = prompt("Ingrese una operacion")
 
-    if(isNaN(numero1) || isNaN(numero2)) {
-        alert("Porfavor ingresar un numero valido")
-    }
+// //     if(isNaN(numero1) || isNaN(numero2)) {
+// //         alert("Porfavor ingresar un numero valido")
+// //     }
 
-    if(numero2 === 0 && operacion === "/"){
-        alert("No se puede dividir entre ceros")
-    }
-}while((isNaN(numero1) || isNaN(numero2)) || (numero2 === 0 && operacion === "/"))
+// //     if(numero2 === 0 && operacion === "/"){
+// //         alert("No se puede dividir entre ceros")
+// //     }
+// // }while((isNaN(numero1) || isNaN(numero2)) || (numero2 === 0 && operacion === "/"))
 
-switch(operacion) {
-    case "+":
-        alert(sumar(numero1, numero2))
-        break
-    case "-":
-        alert(restar(numero1, numero2))
-        break
-    case "*":
-        alert(multiplicar(numero1, numero2))
-        break
-    case "/":
-        alert(dividir(numero1, numero2))
-        break
-    default:
-        alert("Operacion no valida")
-        break
-}
+// switch(operacion) {
+//     case "+":
+//         alert(sumar(numero1, numero2))
+//         break
+//     case "-":
+//         alert(restar(numero1, numero2))
+//         break
+//     case "*":
+//         alert(multiplicar(numero1, numero2))
+//         break
+//     case "/":
+//         alert(dividir(numero1, numero2))
+//         break
+//     default:
+//         alert("Operacion no valida")
+//         break
+// }
